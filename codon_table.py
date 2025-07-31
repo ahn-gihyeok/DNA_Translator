9005e560ea66ee1ceb6d033b234e50a588c5cc92
ahn-gihyeok <aghagh0220@khu.ac.kr>
2025-07-31 22:04:16 +0900

    Update codon_table.py

---
+++ codon_table.py
@@ -0,0 +1,88 @@
+# codon -> amino acid
+codon_to_aa = {
+    'TTT': 'F', 'TTC': 'F',
+    'TTA': 'L', 'TTG': 'L', 'CTT': 'L', 'CTC': 'L', 'CTA': 'L', 'CTG': 'L',
+    'ATT': 'I', 'ATC': 'I', 'ATA': 'I',
+    'ATG': 'M',  # 개시코돈
+    'GTT': 'V', 'GTC': 'V', 'GTA': 'V', 'GTG': 'V',
+    'TCT': 'S', 'TCC': 'S', 'TCA': 'S', 'TCG': 'S', 'AGT': 'S', 'AGC': 'S',
+    'CCT': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+    'ACT': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
+    'GCT': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+    'TAT': 'Y', 'TAC': 'Y',
+    'TAA': '*', 'TAG': '*', 'TGA': '*',  # 종결코돈
+    'CAT': 'H', 'CAC': 'H',
+    'CAA': 'Q', 'CAG': 'Q',
+    'AAT': 'N', 'AAC': 'N',
+    'AAA': 'K', 'AAG': 'K',
+    'GAT': 'D', 'GAC': 'D',
+    'GAA': 'E', 'GAG': 'E',
+    'TGT': 'C', 'TGC': 'C',
+    'TGG': 'W',
+    'CGT': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R', 'AGA': 'R', 'AGG': 'R',
+    'GGT': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G'}
+
+# amino acid -> codons
+aa_to_codon = {
+    'F': ['TTT', 'TTC'],
+    'L': ['TTA', 'TTG', 'CTT', 'CTC', 'CTA', 'CTG'],
+    'I': ['ATT', 'ATC', 'ATA'],
+    'M': ['ATG'],
+    'V': ['GTT', 'GTC', 'GTA', 'GTG'],
+    'S': ['TCT', 'TCC', 'TCA', 'TCG', 'AGT', 'AGC'],
+    'P': ['CCT', 'CCC', 'CCA', 'CCG'],
+    'T': ['ACT', 'ACC', 'ACA', 'ACG'],
+    'A': ['GCT', 'GCC', 'GCA', 'GCG'],
+    'Y': ['TAT', 'TAC'],
+    '*': ['TAA', 'TAG', 'TGA'],
+    'H': ['CAT', 'CAC'],
+    'Q': ['CAA', 'CAG'],
+    'N': ['AAT', 'AAC'],
+    'K': ['AAA', 'AAG'],
+    'D': ['GAT', 'GAC'],
+    'E': ['GAA', 'GAG'],
+    'C': ['TGT', 'TGC'],
+    'W': ['TGG'],
+    'R': ['CGT', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'],
+    'G': ['GGT', 'GGC', 'GGA', 'GGG']}
+
+def seq_list(seq):
+    if not isinstance(seq, str):
+        print('잘못된 입력입니다.')
+        return False
+
+    if len(seq) % 3 != 0:
+        print('유효한 CDS가 아닙니다.')
+        return False
+
+    SEQ = seq.upper().replace("\n", "").replace(" ", "")
+    codons = []
+
+    for i in range(0, len(SEQ), 3):
+        codon = SEQ[i:i+3]
+        codons.append(codon)
+
+    return codons
+
+
+def CheckCDS(codons):
+    if not isinstance(codons, list):
+        print('잘못된 입력입니다.')
+        return False
+
+    if codons[0] != 'ATG':
+        print('유효한 CDS가 아닙니다. (Start codon 없음)')
+        return False
+
+    if codons[-1] not in ['TAA', 'TAG', 'TGA']:
+        print('유효한 CDS가 아닙니다. (Stop codon 없음)')
+        return False
+
+    for codon in codons:
+        if codon not in codon_to_aa:
+            print(f'유효하지 않은 codon: {codon}')
+            return False
+
+    return True
+    
+    
